9aeeaa4b0db9dc9efbfc10d2eb938ac7164b9442
AzuriaN <jibv3729@outlook.com>
2021-04-04 16:52:14 -0500

    first deploy in heroku fixed, again

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
+app.use(express.static(__dirname+'/public'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/public/asesoria-angular/index.html'));
+    res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
